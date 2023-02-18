d53c38ada0604d941de268b7c1ccf7567e8117ad
Bridger Tower <bridger@ziondesign.io>
2023-02-18 11:01:22 -0700

    Updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Footer from '@/components/Footer';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en" className=" bg-stone-900 lg:px-8 lg:pt-8">
+		<html lang="en" className="scroll-smooth bg-stone-200 lg:px-8 lg:pt-8">
 			<head />
 			<body>
 				{children}
