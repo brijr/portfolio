f7e1e0d2aeaa2c12febebc00ffa1e3486129cc17
Bridger Tower <bridger@ziondesign.io>
2023-02-19 22:45:03 -0700

    Updated

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -1,5 +1,6 @@
 import dynamic from 'next/dynamic';
 import { Key } from 'react';
+import Projects from '@/components/Projects';
 
 var accessToken = '33f6b249840bf29923d747505622fb27fefa014de97c331c2633ae85c2770b36';
 
@@ -32,23 +33,50 @@ export default async function Design() {
 				{/* Thoughts Section */}
 				<div className="lg:max-w-screen-md">
 					<h2 className="text-4xl mb-12 font-semi-bold ">Why Design?</h2>
+					<p className="mb-4">
+						I have made design a major part of my life for years. I constantly strive to improve the
+						world around me through design.
+					</p>
+					<p>
+						Design shapes how we interact with the world around us. It can help bring clarity and
+						purpose to seemingly mundane objects or experiences. It can also be an avenue for
+						self-expression, allowing us to create something that expresses our values, beliefs, and
+						ideas.
+					</p>
+					<p className="my-4">
+						Design plays an integral role in creating an aesthetic that enhances user experiences,
+						while adding functionality. From the way a chair is designed to provide comfort, to the
+						way a website is designed to be user-friendly and visually appealing, design helps us to
+						interact with objects and experiences in meaningful ways. Design is everywhere and it
+						helps to create a world that we can enjoy, appreciate and learn from.
+					</p>
+					<p className="mt-4 mb-8 text-xl">
+						"Less, but better." <small>Dieter Rams</small>
+					</p>
 				</div>
 				<div className="lg:max-w-screen-md p-4 md:p-8 mb-12 rounded-xl text-lg bg-slate-200">
-					<p className="mb-4 pb-4 border-b-2 border-b-white text-2xl font-medium">
-						What makes up my design philosophy and process?
+					<p className="mb-4 border-b-2 opacity-75 text-2xl font-medium">
+						What makes up my design process?
 					</p>
-					<ul className="list-item">
+					<ul className="list-item md:grid md:grid-cols-2">
+						<li>&rarr; Research</li>
 						<li>&rarr; Branding</li>
 						<li>&rarr; Typography</li>
 						<li>&rarr; Color Theory</li>
 						<li>&rarr; Motion Design</li>
 						<li>&rarr; User Experience Design (UX Design)</li>
-						<li>&rarr; Aesthetics</li>
+						<li>&rarr; User Interface Design (UI Design)</li>
 						<li>&rarr; Design Systems</li>
+						<li>&rarr; Aesthetics</li>
+						<li>&rarr; Minimalism</li>
 						<li>&rarr; Figma and Prototyping</li>
 						<li>&rarr; Wireframing</li>
 						<li>&rarr; Site Architecture</li>
 					</ul>
+				</div>
+
+				<div className="lg:max-w-screen-md mb-12 md:mb-24 md:mt-24">
+					<Projects />
 				</div>
 				{/* Dribbble section */}
 				<div className="lg:max-w-screen-md">
